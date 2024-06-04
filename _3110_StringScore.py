d84c0041899036e68355504fb4a700f711f100b5
Dawid Klimont <dawid.klimont@gmail.com>
2024-06-04 18:48:04 +0200

    a

---
+++ _3110_StringScore.py
@@ -0,0 +1,11 @@
+def scoreOfString(s: str) -> int:
+    index = 0
+    solution = 0
+    while index < len(s)-1:
+        solution += abs(ord(s[index])-ord(s[index+1]))
+        index += 1
+    return solution
+    
+input = "hello"
+output = scoreOfString(input)
+print(output)
