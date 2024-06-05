a328c421de3e0b64339f7d2a982e1232552be589
DawidKlimont <dawid.klimont@gmail.com>
2024-06-05 15:41:28 +0200

    a

--- _2894_IsDivisibleSumDiff.py
+++ _2894_IsDivisibleSumDiff.py
@@ -1,13 +1,11 @@
 def differenceOfSums(n: int, m: int) -> int:
-    index = 1
     num1 = 0
     num2 = 0
-    while index <= n:
-        if index%m == 0:
-            num2+=index
+    for i in range (1,n+1):
+        if i%m == 0:
+            num2+=i
         else:
-            num1+=index
-        index +=1
+            num1+=i
     return num1-num2
 
 print(differenceOfSums(10,3))
--- _3110_StringScore.py
+++ _3110_StringScore.py
@@ -1,9 +1,7 @@
 def scoreOfString(s: str) -> int:
-    index = 0
     solution = 0
-    while index < len(s)-1:
-        solution += abs(ord(s[index])-ord(s[index+1]))
-        index += 1
+    for i in  range(0,len(s)-1):
+        solution += abs(ord(s[i])-ord(s[i+1]))
     return solution
     
 print(scoreOfString("hello"))
