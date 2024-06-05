5099a8d39105912477f2c8d339156860e7468cf6
DawidKlimont <dawid.klimont@gmail.com>
2024-06-05 16:16:51 +0200

    a

---
+++ _2807_InsertGCD.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+from math import gcd
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+
+def insertGreatestCommonDivisors( head: Optional[ListNode]) -> Optional[ListNode]:
+    current = head
+    while current.next:
+        next_current = current.next
+        minDiv = gcd(current.val,next_current.val)
+
+        current.next = ListNode(minDiv, next_current)
+        current = next_current
+    return head
+
+val = [18,6,10,3,69,12,451,515523,1215]
+head = ListNode(val[0])
+prev = head
+for i in range(1,len(val)):
+    cur = ListNode(val[i])
+    prev.next = cur
+    prev = cur
+
+cur = insertGreatestCommonDivisors(head)
+result=[]
+while cur:
+    result.append(cur.val)
+    cur = cur.next
+
+print(result)
