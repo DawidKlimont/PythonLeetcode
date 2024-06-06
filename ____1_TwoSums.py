3981ed32bb35274d7c6a62504bc90fd882a0ec3f
DawidKlimont <dawid.klimont@gmail.com>
2024-06-06 16:10:27 +0200

    funny performance test

--- ____1_TwoSums.py
+++ ____1_TwoSums.py
@@ -1,4 +1,5 @@
 from typing import List
+from time import process_time
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -9,5 +10,22 @@ class Solution:
             else:
                 hashmap[target-valueI] = i
         return [-1]
-    
-print(Solution().twoSum([2,7,11,15],9))
+
+def test(instace: Solution, iteration: int):
+    start = process_time()
+    percent_step = 34
+    step_val = int(iteration/(100/percent_step))
+    for i in range(iteration):
+        if(i%step_val==0) and i>0:
+           print(str(int((i/step_val)*percent_step))+"% done "+str(int(process_time()-start))+" seconds passed") 
+        instance.twoSum([2,7,11,15],9)
+    result = process_time()-start
+    return result
+
+instance = Solution()
+print(instance.twoSum([2,7,11,15],9))
+
+total = 0
+iterations = 10_000_000
+total+=test(instance,iterations)
+print(str(total)+"s in total for "+str(iterations)+" iterations and "+str(total/(iterations))+"s avg")
