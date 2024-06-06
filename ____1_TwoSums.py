c66c53cf00a079b7160ccd04892e61e546db5c12
DawidKlimont <dawid.klimont@gmail.com>
2024-06-06 13:41:40 +0200

    a

--- ____1_TwoSums.py
+++ ____1_TwoSums.py
@@ -1,10 +1,13 @@
 from typing import List
 
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    hashmap = {}
-    for i,valueI in enumerate(nums):
-        if valueI in hashmap:
-            return [hashmap[valueI], i] 
-        else:
-            hashmap[target-valueI] = i
-    return [-1]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i,valueI in enumerate(nums):
+            if valueI in hashmap:
+                return [hashmap[valueI], i] 
+            else:
+                hashmap[target-valueI] = i
+        return [-1]
+    
+print(Solution().twoSum([2,7,11,15],9))
