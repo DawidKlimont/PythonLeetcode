ed649e77320c97b215590b038a0f6cb1cef78eab
DawidKlimont <dawid.klimont@gmail.com>
2024-06-06 22:52:53 +0200

    a

---
+++ _1637_WidestVertical.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        total = []
+        for point in points:
+            total.append(point[0])
+        total.sort()
+        solution = 0
+        for i in range(len(total)-1):
+            new_val=total[i+1]-total[i]
+            solution = max(solution,new_val)
+        return solution
+    
+print(Solution().maxWidthOfVerticalArea([[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]))
